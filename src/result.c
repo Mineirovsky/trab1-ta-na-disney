1fd645e6d168b4f9e939092044bf8d53d4fe8869
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2018-11-26 18:12:48 -0200

    Result functions

---
+++ src/result.c
@@ -0,0 +1,31 @@
+#include "result.h"
+#include "confusion_matrix.h"
+#include "knn.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void result_fprint(Result result, FILE *result_file)
+{
+    fprintf(result_file, "%.2f\n\n", result.accuracy);
+    cm_fprint(&result.cm, result_file);
+    printf("\n");
+
+    for (int i = 0; i < result.samples; i++)
+    {
+        fprintf(result_file, "%.0f\n", result.predictions[i]);
+    }
+}
+
+void result_print(Result result)
+{
+    result_fprint(result, stdout);
+}
+
+void result_delete(Result *result)
+{
+    if (!result->predictions)
+    {
+        free(result->predictions);
+        result->predictions = NULL;
+    }
+}
