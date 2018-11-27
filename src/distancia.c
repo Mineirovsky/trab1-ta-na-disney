f35b4400b1277d60ea676afbe23004db47327acd
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2018-11-27 14:13:32 -0200

    Added comments

--- src/distancia.c
+++ src/distancia.c
@@ -2,14 +2,25 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * @brief Função privada para encontrar o maior valor da lista
+ *
+ * @param n Tamanho da lista
+ * @param list Lista
+ * @return float* Ponteiro para o maior valor
+ */
 float *_distancia_maxf (unsigned int n, float *list)
 {
+    // Ponteiro para armazenar o maior já encontrado
     float *bigger = list;
 
+    // Itera por cada valor da lista
     for (int i = 1; i < n; i++)
     {
-        if (*(list + i) > *bigger)
+        // Procura por valores maiores dos que o já encontrado
+        if (list[i] > *bigger)
         {
+            // Grava o ponteiro do novo maior valor
             bigger = list + i;
         }
     }
@@ -19,28 +30,35 @@ float *_distancia_maxf (unsigned int n, float *list)
 
 float distancia_euclidiana (unsigned int n, float *p, float *q)
 {
+    // Acumulador para o somatório
     float acc = 0.0;
 
+    // Soma as distâncias calculadas em cada eixo
     for (int i = 0; i < n; i++)
     {
         acc += powf(p[i] - q[i], 2.0);
     }
 
+    // Retorna a raiz quadrada do somatório
     return sqrtf(acc);
 }
 
 float distancia_chebyshev (unsigned int n, float *p, float *q)
 {
+    // Vetor para armazenar as distâncias em cada eixo
     float *dist_axis = malloc(sizeof(float) * n);
     float distancia;
 
+    // Calcula e registra as distâncias de cada eixo
     for (int i = 0; i < n; i++)
     {
-        *(dist_axis + i) = fabsf(*(p + i) - *(q + i));
+        dist_axis[i] = fabsf(*(p + i) - *(q + i));
     }
 
+    // Encontra a maior distância dos eixos
     distancia = *(_distancia_maxf(n, dist_axis));
 
+    // Libera o vetor dos eixos
     free(dist_axis);
 
     return distancia;
@@ -48,12 +66,15 @@ float distancia_chebyshev (unsigned int n, float *p, float *q)
 
 float distancia_minkowski (unsigned int n, float *p, float *q, float r)
 {
+    // Acumulador para o somatório
     float acc = 0.0;
 
+    // Soma as distâncias calculadas em cada eixo
     for (int i = 0; i < n; i++)
     {
         acc += powf(fabsf(p[i] - q[i]), r);
     }
 
+    // Retorna a potência normalizada do somatório
     return powf(acc, 1.0 / r);
 }
